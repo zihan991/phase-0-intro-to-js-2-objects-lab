97ddb3c813aac0ed254c182b621dd5ffe4fe58e6
Amie Foster <amie.n.foster@gmail.com>
2022-02-20 15:18:19 -0700

    commit tree 90ebd2d8984e751dcfb8b57ca1e1e95c4dddcc0d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {
+    name: "Max",
+    streetAddress: "5 Main Street",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Alternate using ES6 Spread operators:
+
+    return {...employee, [key]: value};
+    
+  }
