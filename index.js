3efc704d7868f7073036da4555e879c6a76b1610
ittehadzihan <zihan991@gmail.com>
2022-07-21 09:27:08 -0400

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = { 'name' : 'Zihan', 'streetAddress' : '100 Oakland'}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee}
+
+    newObj[key] = value
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]= value 
+
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+   delete employee[key]
+   return employee
+}
